2d4a530535215e072e448bdf226129dcac8bf754
Gayane_1 <gmiranyan1@gmail.com>
2021-09-27 21:50:32 +0400

    initil commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const taskRouter = require("./routes/taskRouter");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/tasks", taskRouter);
+
+app.use(function (req, res) {
+  res.status(404).send("not found");
+});
+
+mongoose.connect("mongodb://localhost:27017/todo").then(() => {
+  app.listen(4000);
+});
---
+++ controllers/taskController.js
@@ -0,0 +1,43 @@
+const Task = require("../model/task");
+
+exports.addTask = function (req, res) {
+  const task = new Task({
+    text: req.body.text,
+  });
+
+  task.save(function (err, doc) {
+    if (err) {
+      res.send(err);
+    }
+    res.send(doc);
+  });
+};
+
+exports.getTasks = function (req, res) {
+  Task.find({}, (err, doc) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send(doc);
+  });
+};
+
+exports.updTasks = function (req, res) {
+  const id = req.params.id;
+  Task.findByIdAndUpdate(id, { status: true }, function (err, doc) {
+    if (err) {
+      res.send(err);
+    }
+    res.send(doc);
+  });
+};
+
+exports.delTasks = function (req, res) {
+  const id = req.params.id;
+  Task.findByIdAndDelete(id, function (err, doc) {
+    if (err) {
+      res.send(err);
+    }
+    res.send(doc);
+  });
+};
